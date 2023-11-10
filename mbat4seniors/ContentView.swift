d69aa42462bebe29763fca094ace98cd6333bdec
Ali Al-Fatlawi <alawi_amer@yahoo.co.uk>
2023-11-10 12:49:01 +0300

    Initial Commit

---
+++ mbat4seniors/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mbat4seniors
+//
+//  Created by Ali Amer on 11/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mbat4seniors/mbat4seniorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mbat4seniorsApp.swift
+//  mbat4seniors
+//
+//  Created by Ali Amer on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct mbat4seniorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
